bea9b3f04e22948518e33f4a48f52b9fb3b53548
Erik Richter <erichter_h@dev.bse.edu>
2015-04-20 16:58:09 -0500

    Stream Specific Data Complete

--- StreamSpecificData.py
+++ StreamSpecificData.py
@@ -6,29 +6,31 @@ from tweepy import Stream
 import json
 
 
-consumer_key="xIvqTwnKncz4MvckyIzIzIsCh"
-consumer_secret="XlJP5xE5OyVyRcJ5MDP20KratI7juDL0KrDYSAKSAzBb0GsrWc"
+consumer_key='xIvqTwnKncz4MvckyIzIzIsCh'
+consumer_secret='XlJP5xE5OyVyRcJ5MDP20KratI7juDL0KrDYSAKSAzBb0GsrWc'
 
-access_token="3164465356-IpWdPyksEN5eTHKKDT8KtCHdjbgaz6sFCQ7Trm2"
-access_token_secret="onubCuaZdM3jEhcFj7HIighRuYcV7gyvhoGjWw2XByFkN"
+access_token='3164465356-IpWdPyksEN5eTHKKDT8KtCHdjbgaz6sFCQ7Trm2'
+access_token_secret='onubCuaZdM3jEhcFj7HIighRuYcV7gyvhoGjWw2XByFkN'
 
 class StdOutListener(StreamListener):
-    """ A listener handles tweets are the received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
-    """
+
     def on_data(self, data):
         tweets = json.loads(data)
-        tweetstext = u''.join(tweets["text"]).encode('utf-16','utf-8')
-        print(tweets["user"]["location"])
-        print(tweets["lang"])
-        print(tweetstext)
-        print(tweets["entities"]["hashtags"])
-        print(tweets["entities"]["trends"])
-        print (" ")
+        tweetstext = ''.join(tweets['text']).encode('utf-8')
+        try:
+            print(tweets['user']['location'])
+            print(tweets['lang'])
+            print(tweetstext)
+            print(tweets['entities']['hashtags'])
+            print(tweets['entities']['trends'])
+        except UnicodeEncodeError:
+            print('UnicodeEncodeError')
+        print (' ')
         return True
 
     def on_error(self, status):
         print(status)
+        return True
 
 if __name__ == '__main__':
     l = StdOutListener()
