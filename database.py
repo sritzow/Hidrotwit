8071bd295bef4724106eaeb7d1f2e098ccc037cd
Marc Galang <mgalang_h@dev.bse.edu>
2015-04-20 16:38:48 -0500

    Initiated a SQL connection in order to begin storing data.

--- database.py
+++ database.py
@@ -2,15 +2,11 @@ import json
 import mongodb
 import pymongo
 
-will be streaming language keywords and hashtags
-tweettable user database to store locations and data
+#will be streaming language keywords and hashtags
+#tweettable user database to store locations and data
 
 def tweettable(self, tweets):
-	self.db = pymongo.MongoClient().tweets#placeholder for sql connection to be changed when set up
+	self.db = pymongo.MongoClient().tweets#creates connection to MongoDB and creates 'tweets' database
 	self.db.tweets.insert(data)#inserts into db
 	data = json.loads(tweets)#passes json into self.db.tweets.insert
 	db.tweets.save(self, tweets)
-
-    
-    
-    
