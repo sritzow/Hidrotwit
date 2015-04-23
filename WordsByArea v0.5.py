d52a3a7581dfdbcb5df5cfd1679929cc83c0c094
sritzow <sritzow_h@dev.bse.edu>
2015-04-23 10:54:49 -0500

    Update WordsByArea v0.5.py
    questions

--- WordsByArea v0.5.py
+++ WordsByArea v0.5.py
@@ -7,18 +7,18 @@ try:
 except pymongo.errors.ConnectionFailure:
 	print "Connection Failed, Womp Womp..."
 
-db = conn.tweets
-items = db.tweets.find()
-words = tweet.text.split(' ')
-words = [0]
+db = conn.tweets #ok
+items = db.tweets.find() #ok
+words = tweet.text.split(' ') #? what is tweet in this case
+words = [0] #?
 
-for word in item[]:
+for word in item[]: #Do you mean items?
 	print item
-	if len(word) in item <= 3:
-		item.remove(word)
+	if len(word) in item <= 3: #you are looping through each word already no need to check if in item
+		item.remove(word) #why remove word?
 		print "Item Removed"
 	else:
-		words.append(word) and words.count(word)
+		words.append(word) and words.count(word) #not sure if this works o.o
 
 print words
 print words.count
