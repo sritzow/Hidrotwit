0051ffb1f39282c5286d7944095af93de9ffda65
Erik Richter <erichter_h@dev.bse.edu>
2015-04-23 14:14:14 -0500

    Need to enter New Location def

--- TagsByArea.py
+++ TagsByArea.py
@@ -1,16 +1,3 @@
-Geo = 'geo'
-
-locations = {
-    'SanFran': {
-        Geo:'enter geodata'},
-    'NewYork': {
-        Geo:'enter geodata'},
-    'LosAng': {
-        Geo:'enter geodata'},
-    'Chic': {
-        Geo:'enter geodata'},
-    }
-
 import pymongo
 
 conn = pymongo.MongoClient()
@@ -23,7 +10,6 @@ def tagsByArea(tweets):
         try:
             hashtags = each['entities']['hashtags']
             for tags in hashtags:
-        
                 if tags['text'] in hashtag_list:
                     hashtag_list[tags['text']] += 1                 
                 else:
@@ -36,3 +22,34 @@ def tagsByArea(tweets):
 tags = tagsByArea(tweets)
 for hashtags in tags:
     print tags[hashtags],hashtags
+
+
+
+'''import pymongo
+
+conn = pymongo.MongoClient()
+db = conn.tweets
+tweets = db.tweets.find()
+
+def tagsByArea(tweets):
+    hashtag_list = {}
+    area = {}
+    for each in tweets:                             
+        try:
+            hashtags = each['entities']['hashtags']
+            for tags in hashtags:
+                if each['place'] != None:
+                    if each['place']['name'] in area:
+                        area = hashtag_list[each['place']['name']]
+                        if tags['text'] in area:
+                            area[tags['text']] += 1                 
+                        else:
+                            area[tags['text']] = 1
+        except:
+            pass
+    return area
+
+
+tags = tagsByArea(tweets)
+for hashtags in tags:
+    print tags[hashtags],hashtags'''
