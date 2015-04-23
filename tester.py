0d77e08ed761496bfa8e7d96a8d64313e1ae3872
sritzow <sritzow_h@dev.bse.edu>
2015-04-23 13:41:31 -0500

    Update tester.py
    word and hash

--- tester.py
+++ tester.py
@@ -7,24 +7,44 @@ items = db.tweets.find()
 for item in items:
 	print item
 		
-import pymongo
-
-conn = pymongo.MongoClient()
-db = conn.tweets
-
-items = db.tweets.find()
-areas={'chicago' : {}, 'la' : {}, 'sanfran' : {}}
-for item in items:
-	user = item['user']['screen_name']
-	location = tweet["place"]['name']	#gets location from tweet dict
-	if location in areas:
-		area = areas[location]
-		if user in area:
-			area[user] += 1
-		else:
-			area[user] = 1
-	else:
-		areas[location] = {user : 1}
-		
-for area in areas:
-	print area
+def countByHash():
+	area = {}
+	for item in items:
+		for hashs in item['entities']['hashtags']:
+			#print hashs['text']
+			if item["place"] != None:
+				if item["place"]['name'] in area:
+					a = area[item["place"]['name']]
+					if hashs['text'] in a:
+						a[hashs['text']] += 1
+					else:
+						a[hashs['text']] = 1
+				else:
+					area[item['place']['name']] = {hashs['text'] : 1}			
+	return area
+	
+def countByWord():
+	area = {}
+	for item in items:
+		text = item['text']
+		if item["place"] != None:
+			if item["place"]['name'] in area:
+				a = area[item["place"]['name']]
+				for word in text.split(" "):
+					word = word.strip(',.@#').lower()
+					if len(word) >= 3 and not word.startswith('\\'):
+						if word in a:
+							a[word] += 1
+						else:
+							a[word] = 1
+			else:
+				area[item['place']['name']] = {}
+				a = area[item["place"]['name']]
+				for word in text.split(" "):
+					word = word.strip(',.@#').lower()
+					if len(word) >= 3 and not word.startswith('\\'):
+						if word in a:
+							a[word] += 1
+						else:
+							a[word] = 1
+	return area
