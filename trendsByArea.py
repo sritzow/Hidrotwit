12f542c5468d76b8628b33eb8e9ebc6f4b2eb28d
Spencer <sritzow_h@dev.bse.edu>
2015-04-24 09:45:45 -0500

    Commit files

--- trendsByArea.py
+++ trendsByArea.py
@@ -1,6 +1,25 @@
 import pymongo
+<<<<<<< HEAD
 
 conn = pymongo.MongoClient()
 db = conn.tweets
 
 items = db.tweets.find()
+=======
+from tester import *
+
+
+masterTrends = {}
+for item in items.saveWord():
+    savedWords = item
+    print savedWords
+    for item in items.saveHash():
+        if savedWords in items.saveHash():
+            masterTrends[item] += 1
+        elif savedWords != items.saveHash():
+            masterTrends[item] = 1
+print masterTrends
+            
+            
+
+>>>>>>> dd411503cac64f1240ca4e379a16962134656d28
