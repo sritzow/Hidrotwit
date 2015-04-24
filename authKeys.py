56dea35eb95c28bb9268f8d3ea61cdb5eb0018cb
Spencer <sritzow_h@dev.bse.edu>
2015-04-24 09:46:30 -0500

    updates

--- authKeys.py
+++ authKeys.py
@@ -5,16 +5,5 @@ consumer_secret =       'GgPYnNQoGiLBRVNz1utSOtH2pIBFnQKlSeNY7K4hHXmespMzjS',
 
 access_token =          '3188001485-f1EYGUwyKDCvmXEANaXFkkl5FXku79X5EqwY3Lc',
 access_token_secret =   'qIcTN3szqcLf3GxAjPGoScrEpkwhoVzMhWIzURVZazXml',
-
-<<<<<<< HEAD
-=======
-#consumer_key =          '18CunHmODkZ4LcJpsJVL13nbq',
-#consumer_secret =       '2fzaAoKvZfqXv6jSrSUxmXHB3pcj5Pz1ww6PKmox3CzE3bh71S', 
-
-
-#access_token =          '3164465356-aD3Ume08nAOWi8TLHRfAFEZPKH48QD5K1mc5gLU',
-#access_token_secret =   'd4QFKCIofsSItsKp2gA33tvY08oPsgGrUQrYZHt0sI0rb',
-
->>>>>>> 473e5a2608e33c02f5949142102440cf7ddcd50d
 )
 
